e226c1e3126ccc70be8d03adc280b736dc307a5a
samnababar <160249991+samnababar@users.noreply.github.com>
2024-10-21 18:41:42 +0500

    Add files via upload

---
+++ Milestone 1/script.ts
@@ -0,0 +1,17 @@
+body {
+    font-family: 'Times New Roman',Times, serif;
+    margin: 20px;
+    padding: 0;
+    background-color: #f0f4f8;
+}
+
+h1{
+    text-align: center;
+    color: #2c3e50;
+}
+
+form {
+    max-width: 600px;
+    margin:auto;
+    background-color: #f0f4f68
+}
